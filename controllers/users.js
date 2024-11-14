607f10c4f2ae70308cebfef8c2fcf78b8b6dd45c
suman <sumanpariary9@gmail.com>
2024-11-15 01:01:58 +0530

    Commit in New Branch

---
+++ controllers/users.js
@@ -0,0 +1,55 @@
+const User = require("../models/user");
+
+module.exports.renderSignupForm = (req, res) => {
+    res.render("users/signup.ejs");
+}
+
+module.exports.signup = async (req, res) => {
+    try {
+        let { username, email, password } = req.body;
+        const newUser = new User({ email, username });
+
+    //     if (!password || !confirmPassword) {
+    //     req.flash("error", "Please fill out all password fields.");
+    //     return res.redirect("/signup");
+    // }
+
+    // if (password !== confirmPassword) {
+    //     req.flash("error", "Passwords do not match.");
+    //     return res.redirect("/signup");
+    // }
+
+        const registeredUser = await User.register(newUser, password);
+        console.log(registeredUser);
+        req.login(registeredUser, (err) => {
+            if (err) {
+                return next(err);
+            }
+            req.flash("success", "Welcome to wanderlust!");
+            res.redirect("/listings");
+        });
+    } catch (e) {
+        req.flash("error", e.message);
+        res.redirect("/signup");
+    }
+}
+
+module.exports.renderLoginForm = (req, res) => {
+    res.render("users/login.ejs");
+}
+
+module.exports.login = async (req, res) => {
+    req.flash("success", "Welcome back to wanderlust! You are logged in!");
+    let redirectUrl = res.locals.redirectUrl || "/listings";
+    res.redirect(redirectUrl);
+}
+
+module.exports.logout = (req, res, next) => {
+    req.logout((err) => {
+        if (err) {
+            return next(err);
+        }
+        req.flash("success", "you are logged out");
+        res.redirect("/listings");
+    });
+};

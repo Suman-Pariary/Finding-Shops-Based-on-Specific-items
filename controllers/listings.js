f17b36ea3b87704e8892a0896cbb200bb7195647
suman <sumanpariary9@gmail.com>
2024-11-15 13:12:19 +0530

    Commit in controller page

--- controllers/listings.js
+++ controllers/listings.js
@@ -47,7 +47,7 @@ module.exports.showListing = async (req, res) => {
         res.redirect("/listings");
     }
     console.log(listing);
-    res.render("listings/Show.ejs", { listing });
+    res.render("listings/show.ejs", { listing });
 }
 
 module.exports.createListing = async (req, res, next) => {
